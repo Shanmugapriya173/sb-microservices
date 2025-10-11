cc957c9c7a91184b1c9382b116c938535939469c
Shanmugapriya <r.shanmugapriya173@gmail.com>
2025-10-11 17:06:30 +0530

    Lecture 109-114

---
+++ code/section8/accounts/src/main/java/com/eazybytes/accounts/service/client/LoansFeignClient.java
@@ -0,0 +1,17 @@
+package com.eazybytes.accounts.service.client;
+
+import com.eazybytes.accounts.dto.LoansDto;
+import jakarta.validation.constraints.Pattern;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient("loans")
+public interface LoansFeignClient {
+
+    @GetMapping("/api/fetch")
+    public ResponseEntity<LoansDto> fetchLoan(@RequestParam
+                                              @Pattern(regexp = "(^$|[0-9]{10})", message = "mobileNumber must be 10 digits")
+                                              String mobileNumber);
+}
