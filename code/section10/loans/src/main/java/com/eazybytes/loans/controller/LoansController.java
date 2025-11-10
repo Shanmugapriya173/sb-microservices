cad3310bb4537357af72b105dd72092c88ce1c0b
Shanmugapriya <r.shanmugapriya173@gmail.com>
2025-11-10 19:00:33 +0530

    Lecture 145

--- code/section10/loans/src/main/java/com/eazybytes/loans/controller/LoansController.java
+++ code/section10/loans/src/main/java/com/eazybytes/loans/controller/LoansController.java
@@ -248,6 +248,8 @@ public class LoansController {
     })
     @GetMapping("contact-info")
     public ResponseEntity<LoansContactInfoDto> getContactInfo() {
+        logger.debug("Invoked Loans /contact-info API");
+//        throw new RuntimeException();
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(loansContactInfo);
